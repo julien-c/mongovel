d6cbde81a11b2ee9d2cc3dbf5f409b43e38b651d
Julien Chaumond <chaumond@gmail.com>
2013-02-17 19:19:27 +0100

    php artisan workbench

---
+++ src/JulienC/Mongovel/MongovelServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace JulienC\Mongovel;
+
+use Illuminate\Support\ServiceProvider;
+
+class MongovelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('julien-c/mongovel');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
