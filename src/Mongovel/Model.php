103976d54cc5ec226a9b29f5d78569d43400a8c1
Julien Chaumond <chaumond@gmail.com>
2013-02-27 20:40:37 +0100

    Static findOne function

--- src/Mongovel/Model.php
+++ src/Mongovel/Model.php
@@ -25,5 +25,39 @@ class Model {
 			static::$collection = strtolower(get_called_class());
 		}
 	}
+	
+	
+	
+	
+	public static function findOne($p)
+	{
+		$model = get_called_class();
+		
+		$instance = new $model;
+		
+		$m = new \MongoClient();
+		$collection = $m->reaaad->book;
+		
+		$result = call_user_func_array(array($collection, 'findOne'), array(array('_id' => new \MongoId($p))));
+		// var_dump($result);
+		$instance->attributes = $result;
+		return $instance;
+	}
+	
+	
+	public static function __callStatic($method, $parameters)
+	{
+		$model = get_called_class();
+		
+		var_dump($method);
+		
+		var_dump($parameters);
+		
+		$m = new \MongoClient();
+		$collection = $m->reaaad->book;
+		var_dump($collection);
+		
+		return call_user_func_array(array($collection, $method), $parameters);
+	}
 }
 
