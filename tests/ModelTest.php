0fb4dc9b2553de1ead2907cf36885fa18cc0913c
Julien Chaumond <chaumond@gmail.com>
2013-03-04 10:57:06 +0100

    Whitespace (tabs, not spaces)

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -1,54 +1,55 @@
 <?php
+
 include '_start.php';
 
 class ModelTest extends MongovelTests
 {
-  public function testCanStaticalyCreateModels()
-  {
-    $model = DummyModel::create();
+	public function testCanStaticalyCreateModels()
+	{
+		$model = DummyModel::create();
 
-    $this->assertInstanceOf('DummyModel', $model);
-  }
+		$this->assertInstanceOf('DummyModel', $model);
+	}
 
-  public function testCanGetAttributes()
-  {
-    $model = new DummyModel(array('foo' => 'bar'));
+	public function testCanGetAttributes()
+	{
+		$model = new DummyModel(array('foo' => 'bar'));
 
-    $this->assertEquals('bar', $model->foo);
-  }
+		$this->assertEquals('bar', $model->foo);
+	}
 
-  public function testCanTransformMongoId()
-  {
-    $model = new DummyModel(array('_id' => 'foo'));
+	public function testCanTransformMongoId()
+	{
+		$model = new DummyModel(array('_id' => 'foo'));
 
-    $this->assertEquals('foo', $model->id);
-  }
+		$this->assertEquals('foo', $model->id);
+	}
 
-  public function testCanTransformToArray()
-  {
-    $model = new DummyModel(array('_id' => '42', 'foo' => 'bar'));
+	public function testCanTransformToArray()
+	{
+		$model = new DummyModel(array('_id' => '42', 'foo' => 'bar'));
 
-    $this->assertEquals(array('id' => '42', 'foo' => 'bar'), $model->toArray());
-  }
+		$this->assertEquals(array('id' => '42', 'foo' => 'bar'), $model->toArray());
+	}
 
-  public function testCanHaveHiddenAttributes()
-  {
-    $model = new DummyModel(array('hidden' => 'foo', 'foo' => 'bar'));
+	public function testCanHaveHiddenAttributes()
+	{
+		$model = new DummyModel(array('hidden' => 'foo', 'foo' => 'bar'));
 
-    $this->assertEquals(array('foo' => 'bar'), $model->toArray());
-  }
+		$this->assertEquals(array('foo' => 'bar'), $model->toArray());
+	}
 
-  public function testCanJsonifyWithToArray()
-  {
-    $model = new DummyModel(array('hidden' => 'foo', 'foo' => 'bar'));
+	public function testCanJsonifyWithToArray()
+	{
+		$model = new DummyModel(array('hidden' => 'foo', 'foo' => 'bar'));
 
-    $this->assertEquals('{"foo":"bar"}', json_encode($model));
-  }
+		$this->assertEquals('{"foo":"bar"}', json_encode($model));
+	}
 
-  public function testCanCallMethodsOnTheCollection()
-  {
-    $results = DummyModel::find();
+	public function testCanCallMethodsOnTheCollection()
+	{
+		$results = DummyModel::find();
 
-    $this->assertEquals(array(array('foo' => 'bar')), $results);
-  }
+		$this->assertEquals(array(array('foo' => 'bar')), $results);
+	}
 }
--- tests/_start.php
+++ tests/_start.php
@@ -1,15 +1,16 @@
 <?php
+
 class DummyModel extends Mongovel\Model
 {
-  protected $hidden = array('hidden');
+	protected $hidden = array('hidden');
 }
 
 class MongovelTests extends PHPUnit_Framework_TestCase
 {
-  public static function setUpBeforeClass()
-  {
-    DummyModel::$collection = Mockery::mock('MongoCollection', function($mock) {
-      $mock->shouldReceive('find')->andReturn(array(array('foo' => 'bar')));
-    });
-  }
+	public static function setUpBeforeClass()
+	{
+		DummyModel::$collection = Mockery::mock('MongoCollection', function($mock) {
+			$mock->shouldReceive('find')->andReturn(array(array('foo' => 'bar')));
+		});
+	}
 }
