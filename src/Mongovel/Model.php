5b0242c3f9d4ee769885e2967a4f97a4b2224f72
Julien Chaumond <chaumond@gmail.com>
2013-06-06 17:12:20 +0200

    Mongovel\Model extending Mongovel never made much sense
    cc @Maxime

--- src/Mongovel/Model.php
+++ src/Mongovel/Model.php
@@ -12,7 +12,7 @@ use MongoId;
 /**
  * A Mongovel model
  */
-class Model extends Mongovel implements ArrayableInterface, JsonableInterface
+class Model implements ArrayableInterface, JsonableInterface
 {
 	/**
 	 * Collection name
@@ -164,6 +164,76 @@ class Model extends Mongovel implements ArrayableInterface, JsonableInterface
 	{
 		return isset($this->attributes[$key]);
 	}
+	
+	////////////////////////////////////////////////////////////////////
+	/////////////////////// SPECIAL CASE QUERIES ///////////////////////
+	////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Returns an instance of the model populated with data from Mongo
+	 *
+	 * @param array $parameters
+	 *
+	 * @return Model|null
+	 */
+	public static function findOne($parameters)
+	{
+		$parameters = static::handleParameters($parameters);
+		if (!is_array($parameters)) {
+			throw new InvalidArgumentException('A mongo query must be an array of conditions, a MongoId, or the string representation for a MongoId');
+		}
+		
+		$results = static::getModelCollection()->findOne($parameters);
+		
+		if ($results) {
+			return static::getModelInstance($results);
+		}
+		else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Find a model or throw an exception.
+	 *
+	 * @param array $parameters
+	 * 
+	 * @return Model
+	 */
+	public static function findOneOrFail($parameters)
+	{
+		if ( ! is_null($model = static::findOne($parameters))) return $model;
+
+		throw new ModelNotFoundException;
+	}
+
+	/**
+	 * Performs a Full text search on this collection, and returns a Collection of Models
+	 * 
+	 * @param  string $q       Search query
+	 * @param  array  $filter  Restrict the results
+	 * 
+	 * @return Collection
+	 */
+	public static function textSearch($q, $filter = array())
+	{
+		$collectionName = static::getModelInstance()->getCollectionName();
+		
+		$search = self::db()->command(array(
+			'text'   => $collectionName,
+			'search' => $q,
+			'filter' => $filter,
+		));
+		
+		$items = array();
+		if (isset($search['results'])) {
+			foreach ($search['results'] as $r) {
+				$items[] = static::create($r['obj']);
+			}
+		}
+		
+		return new Collection($items);
+	}
 
 	////////////////////////////////////////////////////////////////////
 	/////////////////////////// SERIALIZATION //////////////////////////
@@ -243,4 +313,44 @@ class Model extends Mongovel implements ArrayableInterface, JsonableInterface
 		return static::getModelInstance()->getCollection();
 	}
 
+	/**
+	 * Magically handles MongoIds when passed as strings or objects
+	 *
+	 * @param string|array|MongoId $p An array of parameters or a MongoId (string/object)
+	 *
+	 * @return array
+	 */
+	protected static function handleParameters($p)
+	{
+		// Assume it's a MongoId
+		if (is_string($p) && strlen($p) === 24 && ctype_xdigit($p)) {
+			return array('_id' => new MongoId($p));
+		} elseif ($p instanceof MongoId) {
+			return array('_id' => $p);
+		}
+		
+		return $p;
+	}
+
+	/**
+	 * Replaces all occurences of $old as key name by $new
+	 * @param  string $old
+	 * @param  string $new
+	 * @param  array  $array
+	 * @return New array
+	 */
+	protected static function recursiveChangeKeyNames($old, $new, $array)
+	{
+		$result = array();
+		foreach ($array as $key => $value) {
+			if ($key === $old) {
+				$key = $new;
+			}
+			if (is_array($value)) {
+				$value = static::recursiveChangeKeyNames($old, $new, $value);
+			}
+			$result[$key] = $value;
+		}
+		return $result;
+	}
 }
