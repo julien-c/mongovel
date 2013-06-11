4f66ffa3c8fb55aaad39aac94f12f238746ca75c
Arthur Darcet <arthur.darcet@m4x.org>
2013-06-11 10:31:35 +0200

    Config parameter to log the request parameters

--- src/Mongovel/ProfilingHandler.php
+++ src/Mongovel/ProfilingHandler.php
@@ -7,6 +7,10 @@ class ProfilingHandler
 {
 	public function handle(Timer $timer, $model, $method, $parameters, $caller)
 	{
-		Log::info(sprintf('%f %s::%s (%s)', $timer->get(), $model, $method, $caller));
+		$message = sprintf('%f %s::%s (%s)', $timer->get(), $model, $method, $caller);
+		if (Mongovel::getContainer('config')->get('profiling.mongoLogParameters', false)) {
+			$message .= ' ' . json_encode($parameters);
+		}
+		Log::info($message);
 	}
 }
