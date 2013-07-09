395f252046c4743e1ca9df5ee655d36667fe2c96
Maxime Fabre <ehtnam6@gmail.com>
2013-07-09 17:04:49 +0200

    Backport Reaaad/reaaad@23f6dffbe534e9d0de3719fe62140de949ecccaa

--- src/Mongovel/Auth/MongoUserProvider.php
+++ src/Mongovel/Auth/MongoUserProvider.php
@@ -8,6 +8,12 @@ use User;
 
 class MongoUserProvider implements UserProviderInterface
 {
+	/**
+	 * Retrieve a user by their unique identifier.
+	 *
+	 * @param  mixed  $identifier
+	 * @return \Illuminate\Auth\UserInterface|null
+	 */
 	public function retrieveByID($identifier)
 	{
 		$user = User::findOne($identifier);
@@ -15,14 +21,19 @@ class MongoUserProvider implements UserProviderInterface
 		return $user;
 	}
 
-
+	/**
+	 * Retrieve a user by the given credentials.
+	 *
+	 * @param  array  $credentials
+	 * @return \Illuminate\Auth\UserInterface|null
+	 */
 	public function retrieveByCredentials(array $credentials)
 	{
 		$query = $credentials;
 		unset($query['password']);
 
 		if (isset($query['username'])) {
-			$query['username'] = new MongoRegex('/' . $query['username'] . '/i');
+			$query['username'] = new MongoRegex('/^' . $query['username'] . '$/i');
 		}
 
 		$user = User::findOne($query);
@@ -30,7 +41,13 @@ class MongoUserProvider implements UserProviderInterface
 		return $user;
 	}
 
-
+	/**
+	 * Validate a user against the given credentials.
+	 *
+	 * @param  \Illuminate\Auth\UserInterface  $user
+	 * @param  array  $credentials
+	 * @return bool
+	 */
 	public function validateCredentials(UserInterface $user, array $credentials)
 	{
 		$user = $this->retrieveByCredentials($credentials);
